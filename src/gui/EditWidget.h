0ab4f52add7eb81a256c8ddbafe5df8ef8b3b0d8
mtigzoe <22895511+mtigzoe@users.noreply.github.com>
2026-09-22 20:05:38 -0400

    Fix EditEntryWidget access to stacked widget

--- src/gui/EditWidget.h
+++ src/gui/EditWidget.h
@@ -6,14 +6,6 @@
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 2 or (at your option)
  *  version 3 of the License.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef KEEPASSX_EDITWIDGET_H
@@ -44,6 +36,7 @@ public:
     void addPage(const QString& labelText, const QIcon& icon, QWidget* widget);
     bool hasPage(const QWidget* widget) const;
     int pageIndex(const QWidget* widget) const;
+    int currentPageIndex() const;
     void setPageHidden(QWidget* widget, bool hidden);
     void setCurrentPage(int index);
     void setHeadline(const QString& text);
