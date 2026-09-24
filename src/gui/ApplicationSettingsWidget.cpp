634c2725cecd6c33b085544fada5026696a1cdd9
mtigzoe <22895511+mtigzoe@users.noreply.github.com>
2026-09-24 13:48:17 -0400

    Expose Auto-Type shortcut errors to screen readers

--- src/gui/ApplicationSettingsWidget.cpp
+++ src/gui/ApplicationSettingsWidget.cpp
@@ -213,8 +213,12 @@ ApplicationSettingsWidget::ApplicationSettingsWidget(QWidget* parent)
             QString error;
             if (autoType()->registerGlobalShortcut(key, modifiers, &error)) {
                 m_generalUi->autoTypeShortcutWidget->setStyleSheet("");
+                m_generalUi->autoTypeShortcutWidget->setAccessibleDescription({});
             } else {
                 QToolTip::showText(mapToGlobal(rect().bottomLeft()), error);
+                m_generalUi->autoTypeShortcutWidget->setAccessibleDescription(error);
+                QAccessibleEvent alertEvent(m_generalUi->autoTypeShortcutWidget, QAccessible::Alert);
+                QAccessible::updateAccessibility(&alertEvent);
                 StateColorPalette statePalette;
                 auto color = statePalette.color(StateColorPalette::ColorRole::Error);
                 m_generalUi->autoTypeShortcutWidget->setStyleSheet(
