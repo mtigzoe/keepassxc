7fd29ffb3007b4ef69454e4d50b1cef12885a9f3
mtigzoe <22895511+mtigzoe@users.noreply.github.com>
2026-09-24 11:57:32 -0400

    Give shortcut editor dialog an accessible window title

--- src/gui/ShortcutSettingsPage.cpp
+++ src/gui/ShortcutSettingsPage.cpp
@@ -45,6 +45,8 @@ public:
                                             | QDialogButtonBox::RestoreDefaults,
                                         this))
     {
+        setWindowTitle(QObject::tr("Change Shortcut"));
+
         auto* l = new QVBoxLayout(this);
         connect(m_btnBox, &QDialogButtonBox::accepted, this, &QDialog::accept);
         connect(m_btnBox, &QDialogButtonBox::rejected, this, &QDialog::reject);
