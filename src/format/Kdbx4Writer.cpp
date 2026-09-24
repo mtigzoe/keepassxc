c1109afa2bd522589473b9d02aeb11ca37467998
mtigzoe <22895511+mtigzoe@users.noreply.github.com>
2026-09-24 10:05:49 -0400

    Check KDBX4 header signature write

--- src/format/Kdbx4Writer.cpp
+++ src/format/Kdbx4Writer.cpp
@@ -66,7 +66,7 @@ bool Kdbx4Writer::writeDatabase(QIODevice* device, Database* db)
         QBuffer header;
         header.open(QIODevice::WriteOnly);
 
-        writeMagicNumbers(&header, KeePass2::SIGNATURE_1, KeePass2::SIGNATURE_2, db->formatVersion());
+        CHECK_RETURN_FALSE(writeMagicNumbers(&header, KeePass2::SIGNATURE_1, KeePass2::SIGNATURE_2, db->formatVersion()));
 
         CHECK_RETURN_FALSE(
             writeHeaderField<quint32>(&header, KeePass2::HeaderFieldID::CipherID, db->cipher().toRfc4122()));
