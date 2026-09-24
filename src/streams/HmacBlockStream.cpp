72f49c68e78feb2a1ffbf613f76ee05b4593b30a
mtigzoe <22895511+mtigzoe@users.noreply.github.com>
2026-09-24 09:40:14 -0400

    Propagate HMAC stream write errors

--- src/streams/HmacBlockStream.cpp
+++ src/streams/HmacBlockStream.cpp
@@ -80,8 +80,9 @@ void HmacBlockStream::close()
     // Write final block(s) only if device is writable and we haven't
     // already written a final block.
     if (isWritable() && (!m_buffer.isEmpty() || m_blockIndex != 0)) {
-        if (!m_buffer.isEmpty()) {
-            writeHashedBlock();
+        if (!m_buffer.isEmpty() && !writeHashedBlock()) {
+            LayeredStream::close();
+            return;
         }
 
         // write empty final block
@@ -184,7 +185,7 @@ qint64 HmacBlockStream::writeData(const char* data, qint64 maxSize)
     Q_ASSERT(maxSize >= 0);
 
     if (m_error) {
-        return 0;
+        return -1;
     }
 
     qint64 bytesRemaining = maxSize;
